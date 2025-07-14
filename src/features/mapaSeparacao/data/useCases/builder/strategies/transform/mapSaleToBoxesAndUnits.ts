10e5deade6aea88db65805dacfa703c641cd6cd8
carlos <robertorsilva.krlos@gmail.com>
2025-07-14 11:34:56 -0300

    add converter um for PC

--- src/features/mapaSeparacao/data/useCases/builder/strategies/transform/mapSaleToBoxesAndUnits.ts
+++ src/features/mapaSeparacao/data/useCases/builder/strategies/transform/mapSaleToBoxesAndUnits.ts
@@ -2,10 +2,16 @@ import { TransformStrategy } from "./type-transform";
 
 export class MapSaleToBoxesAndUnits implements TransformStrategy {
   transform(item: any) {
-    if (item.averageUnit === "UN" || item.averageUnit === "CJ" || item.averageUnit === "PC" ) {
+    if (
+      item.averageUnit === "UN" ||
+      item.averageUnit === "CJ" ||
+      item.averageUnit === "PC" ||
+      item.averageUnit === "L" ||
+      item.averageUnit === "BL"
+    ) {
       return { ...item, units: item.sale, boxes: 0, sale: 0 };
     } else {
-      return {...item, units: 0, boxes: item.sale, sale: 0 };
+      return { ...item, units: 0, boxes: item.sale, sale: 0 };
     }
   }
 }
